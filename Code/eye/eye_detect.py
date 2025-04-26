c1eac0d9d28f344753e8f5ad3bef873db67a0261
Rahul Arvind <rahul.a2208@gmail.com>
2025-04-26 17:33:40 -0400

    data_loaders

--- Code/eye/eye_detect.py
+++ Code/eye/eye_detect.py
@@ -23,3 +23,24 @@ transform = transforms.Compose([
     transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.ToTensor(),
 ])
+# ------------------ Custom ImageFolder to Filter Classes ------------------ #
+class FilteredImageFolder(ImageFolder):
+    def find_classes(self, directory):
+        classes = [d.name for d in os.scandir(directory) if d.is_dir() and d.name in keep_classes]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+# ------------------ Data Loaders ------------------ #
+train_dir = "/home/ubuntu/deeplearning_project/data/train"
+test_dir = "/home/ubuntu/deeplearning_project/data/test"
+
+train_dataset = FilteredImageFolder(root=train_dir, transform=transform)
+test_dataset = FilteredImageFolder(root=test_dir, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+OUTPUTS_a = len(train_dataset.classes)
+print("Classes used:", train_dataset.classes)
+
