b8829a74771926a4b3f46741eb287e7459dee039
Rahul Arvind <rahul.a2208@gmail.com>
2025-04-26 17:36:13 -0400

    model for eye_detect.py

--- Code/eye/eye_detect.py
+++ Code/eye/eye_detect.py
@@ -43,4 +43,44 @@ test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 OUTPUTS_a = len(train_dataset.classes)
 print("Classes used:", train_dataset.classes)
+# ------------------ Model Definition ------------------ #
+class EyeCNN(nn.Module):
+    def _init_(self):
+        super(EyeCNN, self)._init_()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self.fc = nn.Linear(128, OUTPUTS_a)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(x.size(0), -1)
+        return self.fc(x)
+
+# ------------------ Instantiate Model ------------------ #
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+if pretrained:
+    print("Using pretrained ResNet18...")
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, OUTPUTS_a)
+    model = model.to(device)
+    IMAGE_SIZE = 224  # Update size for ResNet
+else:
+    print("Using custom EyeCNN...")
+    model = EyeCNN().to(device)
+
 
